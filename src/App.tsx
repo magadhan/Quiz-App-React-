576d205bed84e79f19826482350d60d5a6729fd5
magadhan <athukuttan12345@gmail.com>
2024-07-17 19:42:57 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,94 @@
+import Axios from "axios";
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Questionaire from "./Component/Questionaire";
+// import Header from "./Component/Header";
+const API_URL =
+  "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy";
+// "https://opentdb.com/api.php?amount=20&category=18&difficulty=easy&type=multiple";
+
+function App() {
+  const [questions, setQuestions] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showAnswers, setShowAnswers] = useState(false);
+
+  useEffect(() => {
+    Axios.get(API_URL)
+      .then((res) => res.data)
+      .then((data) => {
+        const questions = data.results.map((question) => ({
+          ...question,
+          answers: [
+            question.correct_answer,
+            ...question.incorrect_answers,
+          ].sort(() => Math.random() - 0.5),
+        }));
+        setQuestions(questions);
+      });
+  }, []);
+
+  const handleAnswer = (answer) => {
+    if (!showAnswers) {
+      if (answer === questions[currentIndex].correct_answer) {
+        setScore(score + 1);
+      } else {
+      }
+    }
+    setShowAnswers(true);
+  };
+
+  const handleNextQuestion = () => {
+    setCurrentIndex(currentIndex + 1);
+    setShowAnswers(false);
+  };
+
+  const handlePreviousQuestion = () => {
+    if (currentIndex > 0) setCurrentIndex(currentIndex - 1);
+    setShowAnswers(false);
+  };
+  const restartGame = () => {
+    setScore(0);
+    setCurrentIndex(0);
+    useEffect(() => {
+      Axios.get(API_URL)
+        .then((res) => res.data)
+        .then((data) => {
+          const questions = data.results.map((question) => ({
+            ...question,
+            answers: [
+              question.correct_answer,
+              ...question.incorrect_answers,
+            ].sort(() => Math.random() - 0.5),
+          }));
+          setQuestions(questions);
+        });
+    }, []);
+  };
+  return questions.length > 0 ? (
+    <div className="container">
+      <h2>Score: {score}</h2>
+      <h2>
+        {currentIndex + 1} out of {questions.length} Questions
+      </h2>
+      <button onClick={restartGame} className="next-question">
+        Restart Game
+      </button>
+      {currentIndex >= questions.length ? (
+        <h1>Game Ended, Your Score is {(score / questions.length) * 100}%</h1>
+      ) : (
+        <Questionaire
+          handleAnswer={handleAnswer}
+          showAnswers={showAnswers}
+          handleNextQuestion={handleNextQuestion}
+          handlePreviousQuestion={handlePreviousQuestion}
+          data={questions[currentIndex]}
+        />
+      )}
+    </div>
+  ) : (
+    <div className="container">..Loading</div>
+  );
+}
+
+export default App;
---
+++ src/Component/Questionaire.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+function Questionaire({
+  handleAnswer,
+  showAnswers,
+  handleNextQuestion,
+  handlePreviousQuestion,
+  data: { question, correct_answer, answers },
+}) {
+  return (
+    <>
+      <div className="questionClass">
+        <h1 dangerouslySetInnerHTML={{ __html: question }} />
+      </div>
+      <div className="button-overall">
+        {answers.map((answer, idx) => {
+          const specialClassName = showAnswers
+            ? answer === correct_answer
+              ? "green-button"
+              : "red-button"
+            : "";
+          return (
+            <button
+              className={`normal-button ${specialClassName}`}
+              onClick={() => handleAnswer(answer)}
+              dangerouslySetInnerHTML={{ __html: answer }}
+            />
+          );
+        })}
+      </div>
+      {showAnswers && (
+        <>
+          <br></br>
+          <button onClick={handleNextQuestion} className="next-question">
+            Next Question
+          </button>
+          <br></br>
+          {/* <button onClick={handlePreviousQuestion} className="next-question">
+            Previous Question
+          </button> */}
+        </>
+      )}
+    </>
+  );
+}
+
+export default Questionaire;
